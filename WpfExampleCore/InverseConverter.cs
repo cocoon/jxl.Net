19b2bcec96057db45f82631e93d3e9c1f824e0e4
local <no@no.local>
2026-03-20 16:29:40 +0100

    add missing converter

---
+++ WpfExampleCore/InverseConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace WpfExampleCore
+{
+    public class InverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double v = (double)value;
+            return v == 0 ? 1 : 1 / v;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double v = (double)value;
+            return v == 0 ? 1 : 1 / v;
+        }
+    }
+}
